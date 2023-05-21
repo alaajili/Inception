29c22903cd2d6154bb9f9b4354f18e52503af767
Abderrahmane Laajili <alaajili@e2r10p9.1337.ma>
2023-05-21 17:10:21 +0100

    env variables and wordpress setup

---
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir -p /var/www/html
+
+cd /var/www/html
+
+rm -rf *
+
+# download the wp-cli.phar file
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+# make the wp-cli.phar file executable
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+# download WordPress
+wp core download --allow-root
+
+mv wp-config-sample.php wp-config.php
+
+
+# change some lines in wp-config.php to connect with the database
+
+sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php
+
+sed -i -r "s/username_here/$DB_USER/1" wp-config.php
+
+sed -i -r "s/password_here/$DB_PASSWORD/1" wp-config.php
+
+sed -i -r "s/localhost/mariadb/1" wp-config.php
+
+
+
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PW --admin_email= --allow-root
+
+wp user create $WP_USER $WP_EMAIL
